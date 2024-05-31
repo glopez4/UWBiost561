a786aecd991fe49c7e452e042f59865c4e6239b5
glopez4 <glopez14@uw.edu>
2024-05-31 13:14:58 -0700

    simulation test

--- R/simulation.R
+++ R/simulation.R
@@ -1,38 +1,42 @@
+#' Compute Maximal Partial Clique simulation
+#'
+#' @param num_func number of functions we want to test
+#' @param num_trials number of trials we want to run
+#' @param clique_fraction the fraction of nodes (of the n nodes) that are part of the demo partial clique
+#' @param clique_density the edge density among the nodes in the demo clique
+#'
+#' @return returns a few different outputs
+#' @export
 simulation <- function(num_func, num_trials) {
-    rm(list=ls())
     set.seed(10)
-    library(UWBiost561)
 
     imp_numbers <- 1:num_func
     trials <- num_trials
     alpha_vec <- c(0.5, 0.95)
 
-    # loop over the levels
     level_trial_list <- lapply(alpha_vec, function(alpha){
       print(paste("Value of alpha:", alpha))
 
       # loop over the different trials for this level
       trial_list <- lapply(1:trials, function(trial){
         print(paste("Working on trial:", trial))
-        # to freeze the randomness of adj_mat
-        set.seed(trial)
+        set.seed(trial) # to freeze the randomness of adj_mat
 
         # generate the data
-        data <- generate_partial_clique(n=10)
+        data <- UWBiost561::generate_partial_clique(n = 10, clique_fraction=0.5, clique_edge_density=0.9)
         adj_mat <- data$adj_mat
 
         # loop over the methods for this trial
-        result_lis <- lapply(imp_numbers, function(imp_number){
-
-          # to freeze the randomness of the method
-          set.seed(trial)
+        result_list <- lapply(imp_numbers, function(imp_number){
+          set.seed(trial) # to freeze the randomness of the method
           cat('*')
-          result <- compute_maximal_partial_clique_master(
+          result <- UWBiost561::compute_maximal_partial_clique_master(
             adj_mat = adj_mat,
             alpha = alpha,
             number = imp_number,
             time_limit = 30
           )
+
 
           return(result)
         })
@@ -46,7 +50,6 @@ simulation <- function(num_func, num_trials) {
 
       return(trial_list)
     })
-
     names(level_trial_list) <- paste0("alpha:", alpha_vec)
 
     # it's always useful to save the date and R session info
@@ -54,7 +57,7 @@ simulation <- function(num_func, num_trials) {
     session_info <- devtools::session_info()
 
     save(level_trial_list, # save your results
-         alpha_vec, # save which alphas you used
+         alpha_vec, # save which alphas you used (for convenience)
          date_of_run, session_info,
          file = "~/simulation.RData")
 }
